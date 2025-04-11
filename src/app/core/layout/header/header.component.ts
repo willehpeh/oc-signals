ced9e79e64dc510146122bdc67a5760edb4a8acc
w.alexander <will_alexander@me.com>
2025-04-11 11:59:12 +0200

    Header signal

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   imports: [],
   template: `
 		<div class="header-container">
-			<h1 class="header">Signalize</h1>
+			<h1 class="header">{{ header() }}</h1>
 			<p class="header-tagline">So your tasks aren't a chore to complete</p>
 		</div>
   `,
   styles: ``
 })
 export class HeaderComponent {
+  header = signal('Signalize');
 
+  constructor() {
+    setTimeout(() => {
+      this.header.set('Signalize!');
+    }, 2000);
+
+    setTimeout(() => {
+      this.header.update(header => header + '!');
+    }, 3000);
+  }
 }
