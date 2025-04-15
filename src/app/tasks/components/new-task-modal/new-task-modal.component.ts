b843856b82cf9c5be64fcf3793d8466123d6e5bf
w.alexander <will_alexander@me.com>
2025-04-15 14:31:23 +0200

    Split out new task modal form

--- src/app/tasks/components/new-task-modal/new-task-modal.component.ts
+++ src/app/tasks/components/new-task-modal/new-task-modal.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { ModalService } from '../../services/modal.service';
 import { NewTaskModalHeaderComponent } from './new-task-modal-header/new-task-modal-header.component';
+import { NewTaskModalFormComponent } from './new-task-modal-form/new-task-modal-form.component';
 
 @Component({
   selector: 'app-new-task-modal',
   imports: [
-    NewTaskModalHeaderComponent
+    NewTaskModalHeaderComponent,
+    NewTaskModalFormComponent
   ],
   templateUrl: './new-task-modal.component.html',
   styleUrls: ['./new-task-modal.component.scss']
