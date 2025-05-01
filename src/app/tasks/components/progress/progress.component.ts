e1340650d91c33b66d99a9bfa4dd31fffc312411
w.alexander <will_alexander@me.com>
2025-05-01 22:39:44 +0200

    Finish progress bar activity

--- src/app/tasks/components/progress/progress.component.ts
+++ src/app/tasks/components/progress/progress.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { CompleteAllTasksButtonComponent } from './complete-all-tasks-button/complete-all-tasks-button.component';
 import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 import { TaskService } from '../../services/task.service';
@@ -16,7 +16,7 @@ import { TaskService } from '../../services/task.service';
 					<h2 class="progress-title">Overall Progress</h2>
 					<span class="progress-count">{{ completedTasks() }} of {{ totalTasks() }} tasks completed</span>
 				</div>
-				<app-progress-bar/>
+				<app-progress-bar [progressPercentage]="progressPercentage()"/>
 			</div>
 			<app-complete-all-tasks-button/>
 		</div>
@@ -27,6 +27,7 @@ export class ProgressComponent {
   private taskService = inject(TaskService);
   totalTasks = this.taskService.totalTasks();
   completedTasks = this.taskService.totalCompletedTasks();
+  progressPercentage = computed(() => Math.round(this.completedTasks() / this.totalTasks() * 100));
 
   onCompleteAllTasks() {
     this.taskService.completeAllTasks();
